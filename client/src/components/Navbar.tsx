355af161a1d4dd060ce6363f328a09a8dbe5ce87
Valerie <valerie.ielsch@gmail.com>
2023-06-09 17:27:00 +0200

    update readme - in progress recuperation de isAdmin

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,15 +1,17 @@
 import { Link, Outlet } from "react-router-dom";
 import checkIsAdmin from "../Utils/utils";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
 function Accueil(): JSX.Element {
+  // A retravailler :
   const user = useContext(AuthContext);
-
   const token = localStorage.getItem("token") as string;
-
-  const isAdmin = checkIsAdmin(token);
-
+  const [isAdmin, setIsAdmin] = useState(checkIsAdmin(token));
+  useEffect(() => {}, [isAdmin]);
+  // ////////////
+  // commentaire de David : créer un hook useCheckToken pour récupérer isAdmin depuis le back est la manière la plus sécurisée:
+  // ex:  const role = useCheckToken();
   return (
     <div>
       <nav>
