39f0b0d064619d856aef0f4bbab44f95d3418b5c
ClemDSC <clemence.p14@gmail.com>
2023-07-07 17:34:51 +0200

    home page mobile ok

--- client/src/pages/Accueil.tsx
+++ client/src/pages/Accueil.tsx
@@ -1,23 +1,21 @@
+import { Link } from "react-router-dom";
+import "../style/accueil.css";
+
 function Users(): JSX.Element {
-/////
-//  useEffect
-/////
-
-/////
-//  useState
-/////
-
-/////
-//  Code
-/////
-
-/////
-//  Return
-/////
   return (
-    <div>
-      Bienvenue !!
-    </div>
+    <section className="home-container">
+      <div className="home-main">
+        <h2>
+          Louez nos équipements d'exception,<br/> vivez l'aventure sportive à votre
+          façon !
+        </h2>
+        <h3>Plus de 50 sports disponibles !</h3>
+        <div className="home-button-container">
+          <button><Link to={"/categories"}>Parcourir les catégories</Link></button>
+          <button className="navy"><Link to="/compte">Accéder à mon espace</Link></button>
+        </div>
+      </div>
+    </section>
   );
 }
 
