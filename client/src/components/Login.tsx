67df1a1c5d331b1bbdc762198d48b7944ec9ab82
ClemDSC <clemence.p14@gmail.com>
2023-07-08 20:59:17 +0200

    handle errors into form login

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -3,6 +3,7 @@ import useAuth from "../hooks/useAuth";
 import { Link, useNavigate } from "react-router-dom";
 import { useLoginLazyQuery } from "../generated";
 import Inscription from "../pages/Inscription";
+import * as Yup from "yup";
 
 import "../style/login.css";
 import { CaretLeft, UserCircle } from "@phosphor-icons/react";
@@ -24,18 +25,44 @@ export default function Login() {
     password: "",
   });
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, "L'adresse e-mail est invalide")
+      .required("L'adresse e-mail est obligatoire.")
+      .email("Veuillez entrer une adresse e-mail valide."),
+    password: Yup.string()
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+        "Minimum 8 caractères, au moins une majuscule, une minuscule et un chiffre"
+      )
+      .required("Le mot de passe est obligatoire.")
+      .min(8, "Le mot de passe doit avoir au minimum 8 caractères."),
+  });
+
+  const [errors, setErrors] = useState<Record<string, string>>({});
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (state.email && state.password) {
-      login({
-        variables: {
-          infos: {
-            ...state,
+
+    try {
+      await validationSchema.validate(state, { abortEarly: false });
+
+      if (state.email && state.password) {
+        login({
+          variables: {
+            infos: { ...state },
           },
-        },
+        });
+      }
+    } catch (err:any) {
+      const yupErrors: Record<string, string> = {};
+      err.inner.forEach((validationError: { path: string | number; message: any; }) => {
+        yupErrors[validationError.path] = validationError.message;
       });
+      setErrors(yupErrors);
     }
   };
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setState({ ...state, [e.target.name]: e.target.value });
   };
@@ -60,12 +87,14 @@ export default function Login() {
               type="text"
               placeholder="Email"
             />
+            {errors.email && <p className="login-error-message">{errors.email}</p>}
             <input
               onChange={handleChange}
               name="password"
               type="password"
               placeholder="Mot de passe"
             />
+            {errors.password && <p className="login-error-message">{errors.password}</p>}
             <button disabled={loading} type="submit" name="login">
               Se connecter
             </button>
