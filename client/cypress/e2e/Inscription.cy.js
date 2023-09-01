726195a1470d906bb25ba2d058c31bd0cab44eaa
Valerie <valerie.ielsch@gmail.com>
2023-09-01 11:45:14 +0200

    modif test cypress

--- client/cypress/e2e/Inscription.cy.js
+++ client/cypress/e2e/Inscription.cy.js
@@ -2,13 +2,21 @@
 
 describe("Register", () => {
   it("Une fois inscrit, l'utilisateur est redirigé", () => {
-    cy.visit("http://localhost:3000/inscription");
+    cy.visit("http://localhost:3000/compte");
+
+    cy.get('[data-form-type="email"]').type("test@test.fr");
+
+    cy.get('[data-form-type="password,new"]').type("Azertyuiop01");
+
+    cy.get('[data-form-type="password,confirmation"]').type("Azertyuiop01");
 
     cy.get('[data-button="register"]').type("S'inscrire");
 
     cy.get('[data-button="register"]').click();
 
-    cy.visit("http://localhost:3000/compte/infos");
+    cy.url().should("include", "/compte/infos");
+
+    // cy.visit("http://localhost:3000/compte/infos");
   });
 });
 
