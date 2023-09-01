07c097df46b9c973ee7cbda6c6d502ead7d592fa
Valerie <valerie.ielsch@gmail.com>
2023-09-01 19:51:05 +0200

    init swiper

--- client/src/pages/ProductSheet.tsx
+++ client/src/pages/ProductSheet.tsx
@@ -3,9 +3,17 @@ import { useQuery } from "@apollo/client";
 import { GET_PRODUCT } from "../graphql/product.query";
 import { Product } from "../generated";
 // style
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// *************
+//Slider slick
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+//************** */
+// import Swiper JS
+import Swiper from "swiper";
+// import Swiper styles
+import "swiper/css";
+
 import "../style/productSheet.css";
 import { GET_PRODUCT_IMAGES } from "../graphql/image.query";
 
@@ -43,27 +51,50 @@ function ProductSheet() {
 
   const product: Product = productInfos.product || {};
 
-// settings Slider
-  var settings = {
-    customPaging: function (i: any) {
-      return (
-        // eslint-disable-next-line jsx-a11y/anchor-is-valid
-        <a>
-          <img
-            height="110px"
-            width="150px"
-            src={`${productImages[i]?.name}`}
-            alt="équipement"
-          />
-        </a>
-      );
+  // settings Slider
+  // var settings = {
+  //   customPaging: function (i: any) {
+  //     return (
+  //       // eslint-disable-next-line jsx-a11y/anchor-is-valid
+  //       <a>
+  //         <img
+  //           height="110px"
+  //           width="150px"
+  //           src={`${productImages[i]?.name}`}
+  //           alt="équipement"
+  //         />
+  //       </a>
+  //     );
+  //   },
+  //   dots: true,
+  //   dotsClass: "slick-dots slick-thumb",
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  // };
+
+  //Swiper
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "vertical",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
     },
-    dots: true,
-    dotsClass: "slick-dots slick-thumb",
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
 
   // Handle functions
   const handleReservation = () => {
@@ -79,13 +110,31 @@ function ProductSheet() {
         <button className="secondary" onClick={handleGoBack}>
           Retour
         </button>
-        <Slider {...settings}>
+        {/* <Slider {...settings}>
           {productImages.map(
             (image: { id: string; name: string; isMain: boolean }) => (
               <img key={image.name} src={image.name} alt={image.name} />
             )
           )}
-        </Slider>
+        </Slider> */}
+
+        <div className="swiper">
+          <div className="swiper-wrapper">
+            {productImages.map(
+              (image: { id: string; name: string; isMain: boolean }) => (
+                <div className="swiper-slide">
+                  <img key={image.name} src={image.name} alt={image.name} />
+                </div>
+              )
+            )}
+          </div>
+          <div className="swiper-pagination"></div>
+
+          <div className="swiper-button-prev"></div>
+          <div className="swiper-button-next"></div>
+
+          <div className="swiper-scrollbar"></div>
+        </div>
       </div>
       <div className="text-container">
         <h1>{product.name}</h1>
